06ad27a30ad0b92381a3b872163019a7669bf272
Jun Ma <majun612@hotmail.com>
2017-02-14 00:05:33 -0600

    finish base page and fucntion without link

--- main.py
+++ main.py
@@ -15,11 +15,61 @@
 # limitations under the License.
 #
 import webapp2
+import jinja2
+import os
+
+from google.appengine.ext import db
+
+template_dir = os.path.join(os.path.dirname(__file__), "templates")
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
+                               autoescape = True)
+class Blogs(db.Model):
+    title = db.StringProperty(required = True)
+    context = db.StringProperty(required = True)
+    created = db.DateTimeProperty(auto_now_add = True)
+    last_modified = db.DateTimeProperty(auto_now = True)
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+
+        fiveposts = db.GqlQuery("SELECT * FROM Blogs ORDER BY created DESC LIMIT 5")
+        t = jinja_env.get_template("front.html")
+        content = t.render(blogs = fiveposts, error = self.request.get("error"))
+        self.response.write(content)
+
+class NewPost(webapp2.RequestHandler):
+    def get(self):
+        t = jinja_env.get_template("newpost.html")
+        content = t.render()
+        self.response.write(content)
+    def post(self):
+        blogtitle = self.request.get("blog_title")
+        blogcontext = self.request.get("blog_context")
+        if blogtitle and blogcontext :
+            blog = Blogs(title = blogtitle, context = blogcontext)
+            blog.put()
+            self.redirect("/")
+        else:
+            error = "We need both a title and a body!"
+            t = jinja_env.get_template("newpost.html")
+            content = t.render(error = error)
+            self.response.write(content)
+
+
+class BlogDetail(webapp2.RequestHandler):
+    def get(self, blog_id):
+        blog = blogs.get_by_id(blog_id)
+        if not blog:
+            self.renderError(404)
+        t = jinja_env.get_template("blog_detail.html")
+        content = t.render(blog=blog)
+
+        self.response.write(content)
+
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainHandler),
+    ('/newpost', NewPost),
+    webapp2.Route('/blogs/<blog_id:\d+>', BlogDetail),
+
 ], debug=True)
